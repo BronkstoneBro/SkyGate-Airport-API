3a16b0cbaaa33f6b5cbcc410c9e9e7dbc1d5e637
Бронислав <atlantwindow@gmail.com>
2025-05-14 14:44:15 +0300

    fix bugs with all admin panel and add route

--- airports/admin.py
+++ airports/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from airports.models import Airport, Route
 
-# Register your models here.
+
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "closest_big_city")
+    search_fields = ("name", "closest_big_city")
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ("id", "source", "destination", "distance")
+    search_fields = ("source__name", "destination__name")
