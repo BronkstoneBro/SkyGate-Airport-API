61367ef71d110e365a395b5200cf8b3ea4282b55
Бронислав <atlantwindow@gmail.com>
2025-05-14 14:13:50 +0300

    Add admin customization for Ticket model: display, filters, search, ordering

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from tickets.models import Ticket
 
-# Register your models here.
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "flight", "seat", "price")
+    list_filter = ("flight",)
+    search_fields = ("flight__airplane__name", "seat")
+    ordering = ("flight", "seat")
