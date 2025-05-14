e435d5a0e642fd235c8af059613ec8b9b56e33e5
Бронислав <atlantwindow@gmail.com>
2025-05-14 14:14:52 +0300

    Add admin customization for Flight model: display fields, filters, search, ordering

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from flights.models import Flight
 
-# Register your models here.
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "airplane",
+        "route",
+        "departure_time",
+        "arrival_time",
+        "status",
+    )
+    list_filter = ("status", "departure_time")
+    search_fields = (
+        "airplane__name",
+        "route__departure__name",
+        "route__arrival__name",
+    )
+    ordering = ("-departure_time",)
